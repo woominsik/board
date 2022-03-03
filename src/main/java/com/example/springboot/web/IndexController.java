50f0738d5d58422875de7c76080857fe67332b3d
Woo Minsik <9737638@naver.com>
2022-03-03 23:40:08 +0900

    "finish google login"

--- src/main/java/com/example/springboot/web/IndexController.java
+++ src/main/java/com/example/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.example.springboot.web;
 
+import com.example.springboot.config.auth.LoginUser;
+import com.example.springboot.config.auth.dto.SessionUser;
 import com.example.springboot.service.PostsService;
 import com.example.springboot.web.dto.PostsResponseDto;
 import lombok.Getter;
@@ -9,15 +11,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 @RequiredArgsConstructor
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts",postsService.findAllDesc());
+
+        if(user!=null){
+            model.addAttribute("userName", user.getName());
+        }
+
         return "index";
     }
 
