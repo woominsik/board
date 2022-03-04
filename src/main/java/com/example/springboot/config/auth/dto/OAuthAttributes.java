a1648e0cdef9e8d794ab4ac4f1da214451d022db
Woo Minsik <9737638@naver.com>
2022-03-04 20:31:38 +0900

    "finish google & naver login"

--- src/main/java/com/example/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/example/springboot/config/auth/dto/OAuthAttributes.java
@@ -26,12 +26,40 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String,Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
+        return ofGoogle("id",attributes);
+        //        return OAuthAttributes.builder()
+//                .name((String)attributes.get("name"))
+//                .email((String)attributes.get("email"))
+//                .picture((String)attributes.get("picture"))
+//                .attributes(attributes)
+//                .nameAttributeKey(userNameAttributeName)
+//                .build();
+    }
+
+    private static OAuthAttributes ofGoogle(String usernameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
         return OAuthAttributes.builder()
-                .name((String)attributes.get("name"))
-                .email((String)attributes.get("email"))
-                .picture((String)attributes.get("picture"))
-                .attributes(attributes)
-                .nameAttributeKey(userNameAttributeName)
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(usernameAttributeName)
+                .build();
+    }
+
+    private static OAuthAttributes ofNaver(String usernameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String)response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(usernameAttributeName)
                 .build();
     }
 
--- src/test/java/com/example/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/example/springboot/web/PostsApiControllerTest.java
@@ -4,29 +4,35 @@ import com.example.springboot.domain.posts.Posts;
 import com.example.springboot.domain.posts.PostsRepository;
 import com.example.springboot.web.dto.PostsSaveRequestDto;
 import com.example.springboot.web.dto.PostsUpdateRequestDto;
-import org.assertj.core.api.Assertions;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.After;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+// For mockMvc
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class PostsApiControllerTest {
+public class PostsApiControllerTest {
 
     @LocalServerPort
     private int port;
@@ -37,31 +43,52 @@ class PostsApiControllerTest {
     @Autowired
     private PostsRepository postsRepository;
 
+    @Autowired
+    private WebApplicationContext context;
+
+    private MockMvc mvc;
+
+    @Before
+    public void setup() {
+        mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+    }
+
     @After
-    public void tearDown () throws Exception{
+    public void tearDown() throws Exception {
         postsRepository.deleteAll();
     }
-    @Test
-    public void Posts_등록된다()throws Exception{
-        String title ="title";
-        String content = "content";
 
+    @Test
+    @WithMockUser(roles="USER")
+    public void Posts_등록된다() throws Exception {
+        //given
+        String title = "title";
+        String content = "content";
         PostsSaveRequestDto requestDto = PostsSaveRequestDto.builder()
                 .title(title)
                 .content(content)
-                .author("우민식")
+                .author("author")
                 .build();
-        String url = "http://localhost:"+port+"/api/v1/posts";
 
-        ResponseEntity<Long> responseEntity =restTemplate.postForEntity(url,requestDto,Long.class);
+        String url = "http://localhost:" + port + "/api/v1/posts";
 
-        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        //when
+        mvc.perform(post(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
+
+        //then
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(title);
         assertThat(all.get(0).getContent()).isEqualTo(content);
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void Posts_수정된다() throws Exception {
         //given
         Posts savedPosts = postsRepository.save(Posts.builder()
@@ -82,14 +109,14 @@ class PostsApiControllerTest {
         String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
 
         //when
-        HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
-
-        ResponseEntity<Long> responseEntity = restTemplate.exchange(url,HttpMethod.PUT,requestEntity,Long.class);
+        mvc.perform(put(url)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(requestDto)))
+                .andExpect(status().isOk());
 
         //then
         List<Posts> all = postsRepository.findAll();
         assertThat(all.get(0).getTitle()).isEqualTo(expectedTitle);
         assertThat(all.get(0).getContent()).isEqualTo(expectedContent);
     }
-
 }
